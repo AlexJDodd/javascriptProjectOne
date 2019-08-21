8c953623608f06b0024af9dcc65d8963e6159eae
helloalexdodd <hello@alexdodd.ca>
2019-08-21 12:55:57 -0400

    working on refactoring

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-Just a simple memory game for week one of my Javascript course. Try it out!
+# Memory Flip Card Game
 
-https://helloalexdodd.github.io/javascriptProjectOne/
+### Live Link
+
+http://alexdodd.ca/memory-game/
+
+Thanks for checking out my memory game! This was my first project in my first JavaScript course. I've done dome refactoring since, and am always working on improving projects. Feel free to clone the app and send recommendations for improvements.
--- script.js
+++ script.js
@@ -1,90 +1,93 @@
-$(document).ready(() => {
+const memory = {
+	$fullList: $('ul'),
+	$listItem: $(`li`),
+	$counter: $(`.counter`),
+	count: $('ul li').length,
+	counter: 0
+}
 
-	let userName = prompt(`Hello and welcome to my memory game! Please enter your name and proceed to uncover the cards to see how quickly you can find all of the pairs!`);
+memory.getUserName = () => {
+	memory.userName = prompt(`Hello and welcome to my memory game! Please enter your name and proceed to uncover the cards to see how quickly you can find all of the pairs!`)
+	!memory.userName ? memory.userName = `friend` : null
+	return memory.userName
+}
 
-	const list = $('ul');
-	const count = $('ul li').length;
-
+memory.randomizeTiles = () => {
+	const { count, $fullList } = memory
 	for (let i = 1; i < count; i++) {
-		let j = Math.floor(Math.random() * count);
-		$('li', list).eq(j).appendTo(list);
-	};
+		let num = Math.floor(Math.random() * count)
+		$('li', $fullList).eq(num).appendTo($fullList)
+	}
+}
 
-	let counter = 0;
-		
-	$("ul li").on('click', function() {
-		counter = counter + 1;
-		$('.counter').text(counter);
+memory.alertWinner = (winnerMessage) => {
+	const { matched } = memory
+	setTimeout(() => {
+		const reload = confirm(`${winnerMessage}`)
+		reload ? window.location.reload() : null 
+	}, 2000)
+	matched.addClass('winner')
+}
 
-		$(this).addClass("clicked");
-		
-		let clickedSymbols = $('li').filter('.clicked');
-			
-		if(clickedSymbols[0].type === clickedSymbols[1].type) {
-
-			$(clickedSymbols).removeClass("clicked").addClass("celebrate");
-			setTimeout(function() {
-				$(clickedSymbols).addClass("matched");
-			}, 900);
-		
-			let matchedSymbols = $('li').filter('.celebrate');
-
-			let winnerAlert = (winnerMessage) => {
-				setTimeout(function() {
-					let reload = confirm(`${winnerMessage}`)
-					if (reload) {
-						window.location.reload();
-					} else {				  
-					};
-					}, 2000);
-				$(matchedSymbols).addClass('winner');
-			}
-
-			if (matchedSymbols.length == 20 && counter <= 20) {	
-				let winnerMessage = `Congratulations, ${userName}! You've beaten my memory game with a perfect score! How is that even possible?! I recommend you stop cheating.`
-				winnerAlert(winnerMessage);
-			}
-
-			else if (matchedSymbols.length == 20 && counter <= 30) {
-				let winnerMessage = `Congratulations, ${userName}! You've beaten my memory game, and in only ${counter} clicks! Amazing job! Want to try again?`
-				winnerAlert(winnerMessage);
-			}
-
-			else if (matchedSymbols.length == 20 && counter <= 40) {
-				let winnerMessage = `Congratulations, ${userName}! You've beaten my memory game, and it only took you ${counter} clicks. Good job, but can you do it in even less?`
-				winnerAlert(winnerMessage);
-			}
-
-			else if (matchedSymbols.length == 20 && counter > 40) {	
-				let winnerMessage = `Congratulations, ${userName}! You've beaten my memory game! But it took you ${counter} clicks. Maybe you want to try again?`
-				winnerAlert(winnerMessage);
-			}
+memory.assessWinner = () => {
+	const { userName, counter, matched, alertWinner } = memory
+	let winnerMessage = ``
+	if (matched.length === 20) {
+		if (counter <= 20) {
+			winnerMessage = `Congratulations, ${userName}! You've beaten my memory game with a perfect score! How is that even possible?! I recommend you stop cheating.`
+		} else if (counter <= 30) {
+			winnerMessage = `Congratulations, ${userName}! You've beaten my memory game, and in only ${counter} clicks! Incredible job! Want to try again?`
+		} else if (counter <= 40) {
+			winnerMessage = `Congratulations, ${userName}! You've beaten my memory game, and it only took you ${counter} clicks. Good job, but can you do it in even less?`
+		} else if (counter > 40) {
+			winnerMessage = `Congratulations, ${userName}! You've beaten my memory game! But it took you ${counter} clicks. Maybe you want to try again?`
 		}
+		alertWinner(winnerMessage);
+	}
+}
 
-		else {
+memory.checkMatch = () => {
+	const { $clicked, assessWinner } = memory
+	if ($clicked[0].type === $clicked[1].type) {
+		$clicked.removeClass("clicked").addClass("celebrate");
+		setTimeout(() => {
+			$clicked.addClass("matched");
+		}, 1000);
+		memory.matched = $('li').filter('.celebrate');
+		assessWinner()
+	}
+	else {
+		$clicked.removeClass("clicked").addClass("wrong")
+		setTimeout(function () {
+			$clicked.removeClass("wrong")
+		}, 1000)
+	}
+}
 
-			$(clickedSymbols).removeClass("clicked").addClass("wrong")
-			setTimeout(function() {
-				$(clickedSymbols).removeClass("wrong")
-			}, 900);
-		}
-	});
+memory.filterClicked = () => {
+	const clicked = memory.$listItem.filter('.clicked')
+	memory.$clicked = $(clicked)
+	clicked.length > 1 ? memory.checkMatch() : null
+}
+
+memory.addCounter = function() {
+	let { counter, $counter, filterClicked } = memory
+	memory.counter = counter + 1
+	$counter.text(memory.counter)
+	$(this).addClass(`clicked`)
+	filterClicked($(this))
+}
+
+memory.onClick = () => {
+	memory.$listItem.on('click', memory.addCounter)
+}
+
+memory.init = () => {
+	memory.randomizeTiles()
+	memory.onClick()
+	memory.getUserName()
+}
+
+$(() => {
+	memory.init();
 });
-	
-//I left my pseudocode here for your interest. I didn't work out exactly like this, but it was my thought process as of Saturday morning.
-
-		//create a listening event for clicking the icons
-		//when clicked run a filter to create a new array of cards with the class of clicked
-		// create an if statement that says that if the new array has less than 2 cards to
-		//change the icon font size and background color to yellow using addClass
-		//create an else if statement that says if the new array has two cards with the class of clicked that have matching id's, change the font size and background color to green using addClass
-		//create an else statement that says that as a last resort, the card (this) and also the previous card you clicked (wtf?) reset the font size and background color using removeClass
-		//create a map that checks the new array for the number of cards
-		//create an if statement that says that if the deck array has the right number of cards, a winning dialogue is created
-		
-		//if time
-		//ask for user's name at the start
-		//congratulate user by name at the end
-
-		//if crazy
-		//make it timed
